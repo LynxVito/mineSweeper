3a0042c18e8aed950fa381473b44a551d6ac6998
LynxVito <mzb508@york.ac.uk>
2023-11-30 18:29:58 +0000

    made the board

--- sketch.js
+++ sketch.js
@@ -1,7 +1,18 @@
+const GRID_SIZE = 30;
+let numberOfBombs;
+
 function setup() {
-    createCanvas(400, 300);
+    createCanvas(GRID_SIZE * 20, GRID_SIZE * 30);
+    numberOfBombs = floor(random(20, 30));
+    console.log(numberOfBombs);
 }
 
 function draw() {
+    background(200);
     
+    for(let i = 0; i < width; i += GRID_SIZE) {
+        for(let j = GRID_SIZE * 5; j < height; j += GRID_SIZE) {
+            square(i, j, GRID_SIZE);
+        }
+    }
 }
