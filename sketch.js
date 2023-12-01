9ed2f377bf27bb3f42ad25e4fb1dee3613db6bc1
LynxVito <146098629+LynxVito@users.noreply.github.com>
2023-12-01 01:58:02 +0000

    click on square disappears
    need to make multiple squares that disappear

--- sketch.js
+++ sketch.js
@@ -1,18 +1,71 @@
 const GRID_SIZE = 30;
-let numberOfBombs;
+let numberOfPresents;
+let squares;
+let mouseIsOverSquare, squareClicked;
+
+class Squares {
+    squareX;
+    squareY;
+
+    constructor(x, y) {
+        this.squareX = x;
+        this.squareY = y;
+    }
+
+    drawSquare() {
+        noStroke();
+        fill(20, 255, 0);
+        square(this.squareX, this.squareY, GRID_SIZE - 5);
+    }
+
+    mouseOverSquare() {
+        if(mouseX < (squareHeight + squareX) && mouseX > squareX && 
+            mouseY > squareY && mouseY < (squareHeight + squareY)) {
+            mouseIsOverSquare = true;
+        }
+        else {
+            mouseIsOverSquare = false;
+        }
+    }
+}
 
 function setup() {
     createCanvas(GRID_SIZE * 20, GRID_SIZE * 30);
-    numberOfBombs = floor(random(20, 30));
-    console.log(numberOfBombs);
+    numberOfPresents = floor(random(20, 30));
+    console.log(numberOfPresents);
+
+    squareClicked = false;
 }
 
 function draw() {
-    background(200);
+    background(200, 40, 20);
+
+    squares = new Squares(GRID_SIZE, GRID_SIZE);
+
+    // if(0 < mouseX < i && 0 < mouseY < j && mouseClicked) {
+    //     squareClicked = true;
+    // }
+
+    // for(let i = 15; i < width - GRID_SIZE + 15; i += GRID_SIZE) {
+    //     for(let j = GRID_SIZE * 5; j < height - GRID_SIZE - 15; j += GRID_SIZE) {
+    //         if(squareClicked === false) {
+    //             fill(180);
+    //             noStroke();
+    //             square(i, j, GRID_SIZE - 5);
+    //         }
+    //         else {
+    //             fill(180, 0);
+    //             noStroke();
+    //             square(i, j, GRID_SIZE - 5);
+    //         }
+    //     }
+    // }
     
-    for(let i = 0; i < width; i += GRID_SIZE) {
-        for(let j = GRID_SIZE * 5; j < height; j += GRID_SIZE) {
-            square(i, j, GRID_SIZE);
-        }
+}
+
+function mousePressed() {
+    if(mouseIsOverSquare === true) {
+        squareClicked = true;
+        fill(20, 0, 255, 0);
     }
 }
