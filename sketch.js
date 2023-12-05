a39db495a9912aa087b52b898539ce50e00abcfc
LynxVito <mzb508@york.ac.uk>
2023-12-05 03:52:11 +0000

    click to open present into coal

--- sketch.js
+++ sketch.js
@@ -1,33 +1,44 @@
 const GRID_SIZE = 30;
 let numberOfPresents;
-let squares;
-let mouseIsOverSquare, squareClicked;
-let squareHeight;
-let squareX, squareY;
+let present;
+let mouseIsOverPresent, presentClicked;
+let presentHeight;
+let presentX, presentY;
+let bluePresent, bluePresentOpening;
+let coal, numberOfCoal;
 
-class Squares {
-    squareX;
-    squareY;
+function preload() {
+    bluePresentOpening = loadImage("assets/present-animation-1.gif");
+    bluePresent = loadImage("assets/blue present.png");
+    coal = loadImage("assets/coal.png");
+}
+
+class Presents {
+    presentX;
+    presentY;
 
     constructor(x, y) {
-        this.squareX = x;
-        this.squareY = y;
+        this.presentX = x;
+        this.presentY = y;
     }
 
-    drawSquare() {
-        noStroke();
-        square(this.squareX, this.squareY, 25);
+    drawpresent() {
+        // noStroke();
+        // present(this.presentX, this.presentY, 25);
 
-        console.log(this.squareX, this.squareY);
+        image(bluePresent, this.presentX, this.presentY,
+            GRID_SIZE, GRID_SIZE);
+
+        // console.log(this.presentX, this.presentY);
     }
 
-    mouseOverSquare() {
-        if(mouseX < (25 + this.squareX) && mouseX > this.squareX && 
-            mouseY > this.squareY && mouseY < (25 + this.squareY)) {
-            mouseIsOverSquare = true;
+    mouseOverpresent() {
+        if(mouseX < (GRID_SIZE + this.presentX) && mouseX > this.presentX && 
+            mouseY > this.presentY && mouseY < (GRID_SIZE + this.presentY)) {
+            mouseIsOverPresent = true;
         }
         else {
-            mouseIsOverSquare = false;
+            mouseIsOverPresent = false;
         }
     }
 }
@@ -37,54 +48,62 @@ function setup() {
     numberOfPresents = floor(random(20, 30));
     console.log(numberOfPresents);
 
-    squareClicked = false;
-    mouseIsOverSquare = false;
+    presentClicked = false;
+    mouseIsOverPresent = false;
 
-    squares = new Squares(0, 0);
-
-    fill(20, 255, 0);
+    present = new Presents(0, 0);
 }
 
 function draw() {
     background(200, 40, 20);
 
-    squares.drawSquare();
-    squares.mouseOverSquare();
+    if(presentClicked === false) {
+        present.drawpresent();
+        present.mouseOverpresent();
+    }
+    else {
+        image(coal, 0, 0, coal.width / 4.5, coal.height /4.5);
+    }
 
-    // for(squareX = 15; squareX < width - GRID_SIZE + 15; squareX += GRID_SIZE) {
-    //     for(squareY = GRID_SIZE * 5; squareY < height - GRID_SIZE - 15; squareY += GRID_SIZE) {
-    //             squares.drawSquare();
-    //             squares.mouseOverSquare();
+    // imageMode(CENTER);
+
+    // image(bluePresent, GRID_SIZE, GRID_SIZE,
+    //     bluePresent.width / GRID_SIZE, bluePresent.height / GRID_SIZE);
+
+    // for(presentX = 15; presentX < width - GRID_SIZE + 15; presentX += GRID_SIZE) {
+    //     for(presentY = GRID_SIZE * 5; presentY < height - GRID_SIZE - 15; presentY += GRID_SIZE) {
+    //             presents.drawpresent();
+    //             presents.mouseOverpresent();
     //         }
     //     }
     // }
 
-    // squareX = 0;
-    // squareY = 0;
-    // squareHeight = GRID_SIZE - 5;
+    // presentX = 0;
+    // presentY = 0;
+    // presentHeight = GRID_SIZE - 5;
 
     // noStroke();
-    // square(squareX, squareY, squareHeight);
+    // present(presentX, presentY, presentHeight);
 
-    // mouseOverSquare();
+    // mouseOverpresent();
 
-    // console.log(mouseIsOverSquare, mouseIsPressed);
+    // console.log(mouseIsOverpresent, mouseIsPressed);
     
 }
 
-// function mouseOverSquare() {
-//     if(mouseX < (squareHeight + squareX) && mouseX > squareX && 
-//         mouseY > squareY && mouseY < (squareHeight + squareY)) {
-//         mouseIsOverSquare = true;
+// function mouseOverpresent() {
+//     if(mouseX < (presentHeight + presentX) && mouseX > presentX && 
+//         mouseY > presentY && mouseY < (presentHeight + presentY)) {
+//         mouseIsOverpresent = true;
 //     }
 //     else {
-//         mouseIsOverSquare = false;
+//         mouseIsOverpresent = false;
 //     }
 // }
 
 function mousePressed() {
-    if(mouseIsOverSquare === true) {
-        squareClicked = true;
-        fill(20, 0, 255, 0);
+    if(mouseIsOverPresent === true) {
+        presentClicked = true;
+        // fill(20, 0, 255, 0);
     }
 }
